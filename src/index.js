a78a2bb92bbe5bf1c65ecbd25b03b6f145570f06
Mikhail Kaladzianets <68966313+Mikhail1990-BY@users.noreply.github.com>
2020-10-03 12:48:52 +0300

    feat: add resolutions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
 exports.min = function min (array) {
+  let minNumber = array;
+  if (minNumber === undefined || array.length === 0) {
   return 0;
+  } else 
+  return Math.min.apply(null, array);
 }
 
 exports.max = function max (array) {
+  let maxNumber = array;
+  if (maxNumber === undefined || array.length === 0) {
   return 0;
+  } else 
+  return Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
+  let avgNumber = array;
+  if (avgNumber === undefined || array.length === 0) {
   return 0;
+  } else
+  var sum = 0;
+  array.forEach(function(value) {
+    sum +=value;
+  });
+  var avg = sum/array.length;
+  return avg;
 }
